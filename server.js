c01846033e4c04d33fb84090187aeffd2a216d6d
Josue <Josue@DESKTOP-NKF5GC8>
2024-06-27 19:20:14 -0600

    Add method post in the server

--- server.js
+++ server.js
@@ -2,25 +2,30 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 5001;
+const PUBLIC = path.join(__dirname,'public');
 
 app.use(express.urlencoded({ extended: true}));
+app.use(express.static(PUBLIC));
 
 app.get('/', (req, res) => {
     console.log('Loading Home...');
-    res.send('Hello World');
+    res.sendFile(path.join(PUBLIC,'index.html'));
+});
+
+app.post('/contactus', (req, res) => {
+    console.info('Contact Us has been called...\n');
+    const name = req.body.name;
+    const email = req.body.email;
+    const subject = req.body.subject;
+    
+    console.log('Form Data:' + 
+                '\nName: ' + name + 
+                '\nEmail: ' + email + 
+                '\nSubject: ' + subject);
+
+    res.send('Data received👌');
 });
 
 app.listen(PORT, () => {
     console.log('Server is running on port' + PORT + '🚴‍♀️')
 });
-
-let Seyf = {
-    Name: 'Michael',
-    LastName: 'Mendez',
-    Age: '19',
-    Direction: 'Me la pela',
-    Game: 'Fornais'
-}
-
-console.log(Seyf.Game);
-
